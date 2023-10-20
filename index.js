549a14606e4c102fa91ed68efb9823b76d5db3e0
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-20 16:23:14 +0600

    vercel deployment again

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
 
 // middleware 
 app.use(cors());
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         // create mongoDB database for Products
         const brandCollections = client.db("productsDB").collection("brands");
@@ -135,7 +135,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
